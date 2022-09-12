b30f85d65689195d8c52fc082bcaf0f0668276bb
Stanly Sukmajaya Kwok <stanlyskwok@gmail.com>
2022-09-12 17:22:24 +0700

    checkpoint for moralis database backend

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,12 @@ import '../styles/globals.css';
 import Header from '../components/Header';
 import { NotificationProvider } from 'web3uikit';
 
+const APP_ID = process.env.NEXT_PUBLIC_APP_ID;
+const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
+
 function MyApp({ Component, pageProps }) {
   return (
-    <MoralisProvider initializeOnMount={false}>
+    <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
       <NotificationProvider>
         <Header />
         <Component {...pageProps} />
